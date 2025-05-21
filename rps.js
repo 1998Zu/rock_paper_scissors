4fe5488508d36224aa16d7e79d27724e3d7f6f9d
Zuzizwe <207649087+1998Zu@users.noreply.github.com>
2025-05-21 07:38:23 +0200

    I've finished my rock_paper_scissors game with 5 rounds and a scoring system.

--- rps.js
+++ rps.js
@@ -9,24 +9,7 @@ function getComputerChoice() {
     return Math.floor(Math.random() * 3) + 1;
 }
 
-userInput = prompt("Choose Your Fate Challenger Rock, Paper or Scissor? Enter Exit To Chicken Out");
-
-function gethumanChoice(input) { 
-    input = input.toLowerCase()
-    if (input == "rock") {
-        return 1;
-    }  else if (input == "paper") {
-        return 2
-    }   else if (input == "scissors") {
-        return 3;
-    }   else {
-        return null
-    }
-}
-
-
-let humanChoice = gethumanChoice(userInput)
-let computerChoice = getComputerChoice();
+  
 
 let scores = {
  draw: 0,
@@ -34,7 +17,6 @@ let scores = {
  computerScore: 0,
 };
 
-let continueLoop = true;
 
 function playRound(humanChoice, computerChoice) { 
     if (humanChoice === 1 && computerChoice == 2){
@@ -60,45 +42,40 @@ function playRound(humanChoice, computerChoice) {
         console.log("Draw. Try Again")
     } 
 } 
-/*playRound(humanChoice, computerChoice);*/
+
 
 
 
 function playGame(){
     for (let i = 0; i < 5; i++){
-      
-        if (humanChoice === 1 && computerChoice == 2){
-            scores.computerScore++;
-            console.log("You lose. Paper beats Rock.")
-        } else if (humanChoice === 1 && computerChoice == 3){
-            scores.humanScore++;
-            console.log("Congratulations!! YOU ROCKED THAT");
-        } else if (humanChoice === 2 && computerChoice == 1){
-            scores.humanScore++
-            console.log("Congratulations!! YOU WON");
-        } else if (humanChoice === 2 && computerChoice == 3){
-            scores.computerScore++
-            console.log("You lose. Scissors cut deep.");
-        } else if (humanChoice === 3 && computerChoice == 1){
-            scores.computerScore++
-            console.log("You lose. Crushing Blow.");
-        } else if (humanChoice === 3 && computerChoice == 2){
-            scores.humanScore++
-            console.log("Congratulations!! YOU SLICED IT UP")
-        } else {
-            scores.draw++
-            console.log("Draw. Try Again")
-        } console.log(scores)
         
-    
+        userInput = prompt("Choose Your Fate Challenger Rock, Paper or Scissor? Enter Exit To Chicken Out");
+        let humanChoice = gethumanChoice(userInput);
+        let computerChoice = getComputerChoice();
+        
+        function gethumanChoice(input) { 
+            input = input.toLowerCase()
+            if (input == "rock") {
+                return 1;
+            }  else if (input == "paper") {
+                return 2
+            }   else if (input == "scissors") {
+                return 3;
+            }   else {
+                return null
+            }
+        }
 
-    }
+     playRound(humanChoice, computerChoice); 
         
+    }
+        console.log(scores);
 
 
 }
 
-playGame(humanChoice, computerChoice)
+
+ playGame()
 
 
 
