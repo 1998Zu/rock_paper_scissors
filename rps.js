4609cc7831e389fbc91dcecc895ce0d0cb1459ed
Zuzizwe <207649087+1998Zu@users.noreply.github.com>
2025-06-01 23:16:01 +0200

    Finish UI for Rock Paper Scissors game

--- rps.js
+++ rps.js
@@ -1,4 +1,3 @@
-console.log("Helllo World")
 
     let rock = 1;
     let paper = 2;
@@ -7,76 +6,83 @@ console.log("Helllo World")
 
 function getComputerChoice() { 
     return Math.floor(Math.random() * 3) + 1;
-}
-
+} 
   
+ 
+function playRound(humanChoice, computerChoice) { 
 
-let scores = {
- draw: 0,
- humanScore: 0,
- computerScore: 0,
+    if (humanChoice === 1 && computerChoice == 2){
+        scores.computerScore++;
+        resultsDiv.textContent = "You lose. Paper beats Rock.";
+    } else if (humanChoice === 1 && computerChoice == 3){
+        scores.humanScore++;
+        resultsDiv.textContent = "Congratulations!! YOU ROCKED THAT";
+    } else if (humanChoice === 2 && computerChoice == 1){
+        scores.humanScore++
+        resultsDiv.textContent = "Congratulations!! YOU WON";
+    } else if (humanChoice === 2 && computerChoice == 3){
+        scores.computerScore++
+        resultsDiv.textContent = "You lose. Scissors cut deep.";
+    } else if (humanChoice === 3 && computerChoice == 1){
+        scores.computerScore++
+        resultsDiv.textContent = "You lose. Crushing Blow.";
+    } else if (humanChoice === 3 && computerChoice == 2){
+        scores.humanScore++
+        resultsDiv.textContent = "Congratulations!! YOU SLICED IT UP";
+    } else {
+        scores.draw++;
+        resultsDiv.textContent = "Draw. Try Again";
+    }  
+
+        document.querySelector("#humanScore").textContent = scores.humanScore;
+        document.querySelector("#computerScore").textContent = scores.computerScore;
+        document.querySelector("#draw").textContent = scores.draw;
+
+
+        if (scores.humanScore === 5 || scores.computerScore === 5) {
+            rockBtn.disabled = true;
+            paperBtn.disabled = true;
+            scissorsBtn.disabled = true;
+        
+            resultsDiv.textContent += " Game Over.";
+        }
+        
+    }
+
+
+
+ let scores = {
+    draw: 0,
+    humanScore: 0,
+    computerScore: 0,
 };
 
 
-function playRound(humanChoice, computerChoice) { 
-    if (humanChoice === 1 && computerChoice == 2){
-        scores.computerScore++;
-        console.log("You lose. Paper beats Rock.")
-    } else if (humanChoice === 1 && computerChoice == 3){
-        scores.humanScore++;
-        console.log("Congratulations!! YOU ROCKED THAT");
-    } else if (humanChoice === 2 && computerChoice == 1){
-        scores.humanScore++
-        console.log("Congratulations!! YOU WON");
-    } else if (humanChoice === 2 && computerChoice == 3){
-        scores.computerScore++
-        console.log("You lose. Scissors cut deep.");
-    } else if (humanChoice === 3 && computerChoice == 1){
-        scores.computerScore++
-        console.log("You lose. Crushing Blow.");
-    } else if (humanChoice === 3 && computerChoice == 2){
-        scores.humanScore++
-        console.log("Congratulations!! YOU SLICED IT UP")
-    } else {
-        scores.draw++
-        console.log("Draw. Try Again")
-    } 
-} 
+
+  
+
+ const rockBtn = document.querySelector("#rockBtn");
+ rockBtn.addEventListener("click", () => {
+    const computerChoice = getComputerChoice();
+    playRound(1, computerChoice);
+ });
 
 
+const paperBtn = document.querySelector("#paperBtn");
+paperBtn.addEventListener("click", () => {
+    const computerChoice = getComputerChoice();
+    playRound(2, computerChoice);
+
+});
+
+const scissorsBtn = document.querySelector("#scissorsBtn");
+scissorsBtn.addEventListener("click", () => {
+    const computerChoice = getComputerChoice();
+    playRound(3, computerChoice);
+});
 
 
-function playGame(){
-    for (let i = 0; i < 5; i++){
-        
-        userInput = prompt("Choose Your Fate Challenger Rock, Paper or Scissor? Enter Exit To Chicken Out");
-        let humanChoice = gethumanChoice(userInput);
-        let computerChoice = getComputerChoice();
-        
-        function gethumanChoice(input) { 
-            input = input.toLowerCase()
-            if (input == "rock") {
-                return 1;
-            }  else if (input == "paper") {
-                return 2
-            }   else if (input == "scissors") {
-                return 3;
-            }   else {
-                return null
-            }
-        }
-
-     playRound(humanChoice, computerChoice); 
-        
-    }
-        console.log(scores);
-
-
-}
-
-
- playGame()
-
+const resultsDiv = document.querySelector("#Results");
 
 
 
